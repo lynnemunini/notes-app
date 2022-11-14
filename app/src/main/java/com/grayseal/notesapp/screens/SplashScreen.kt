a0d229c3618614e62231af86b3cdc30b2dd0bdea
lynnemunini <muninilynne65@gmail.com>
2022-11-14 20:34:23 +0300

    Create Welcome screen

--- app/src/main/java/com/grayseal/notesapp/screens/SplashScreen.kt
+++ app/src/main/java/com/grayseal/notesapp/screens/SplashScreen.kt
@@ -4,66 +4,58 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.outlined.DoubleArrow
+import androidx.compose.material.icons.outlined.FiberNew
+import androidx.compose.material.icons.outlined.Fingerprint
+import androidx.compose.material.icons.outlined.Kitesurfing
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.grayseal.notesapp.R
+import com.grayseal.notesapp.navigation.NoteScreens
 import com.grayseal.notesapp.ui.theme.*
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Content()
+    SplashContent(navController = navController)
 }
 
 
 @Composable
-fun Content() {
+fun SplashContent(navController: NavController) {
     val imageModifier = Modifier
         .size(400.dp)
-        .padding(top = 30.dp)
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
         Image(painter = painterResource(id = R.drawable.splash),
             contentDescription = "Splash Screen Image",
             modifier = imageModifier )
         Spacer(modifier = Modifier.height(20.dp))
-        Text("Don't use paper again.", style = (TextStyle(color = Color(0xFF736064), fontSize = 25.sp)), fontFamily = sonoFamily, fontWeight = FontWeight.Bold)
+        Text("Don't use paper again.", style = (TextStyle(color = Color(0xFFdaaac0), fontSize = 25.sp)), fontFamily = sonoFamily, fontWeight = FontWeight.Bold)
         Spacer(Modifier.height(10.dp))
-        Text("Capture life beautifully", style = (TextStyle(fontSize = 18.sp, color = Color.Black)), fontFamily = sonoFamily, fontWeight = FontWeight.ExtraLight)
-        Spacer(Modifier.weight(1f))
-        Next()
+        Text("Organize your notes beautifully", style = (TextStyle(fontSize = 18.sp, color = Color.Black)), fontFamily = sonoFamily, fontWeight = FontWeight.Normal)
+        Spacer(Modifier.height(40.dp))
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 40.dp), horizontalArrangement = Arrangement.End) {
+            NextIconButton(onClick = {navController.navigate(route = NoteScreens.WelcomeScreen.name)})
+        }
     }
 }
 
 @Composable
-fun Next(){
-    val next by remember {
-        mutableStateOf(false)
-    }
-    NextIconButton(next = next, onClick = { /*TODO*/ })
-}
-@Composable
 private fun NextIconButton(
-    next: Boolean,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    onClick: () -> Unit
 ) {
     IconButton(onClick = onClick) {
-        Icon(imageVector = Icons.Filled.ArrowForward, modifier = Modifier.size(40.dp), contentDescription = "Arrow Forward", tint = Color(0xFFcc9054))
+        Icon(imageVector = Icons.Outlined.Fingerprint, modifier = Modifier.size(60.dp), contentDescription = "Fingerprint", tint = Color(0xFFdaaac0))
     }
 }
--- app/src/main/java/com/grayseal/notesapp/screens/WelcomeScreen.kt
+++ app/src/main/java/com/grayseal/notesapp/screens/WelcomeScreen.kt
@@ -1,9 +1,84 @@
 package com.grayseal.notesapp.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.EmojiNature
+import androidx.compose.runtime.*
+import androidx.compose.runtime.R
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.grayseal.notesapp.ui.theme.sonoFamily
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
+    Name()
 
+}
+
+@Composable
+fun Name() {
+    var name by remember{
+        mutableStateOf("")
+    }
+    WelcomeContent(name = name, onNameChange = {name = it})
+}
+
+@Composable
+fun WelcomeContent(name: String, onNameChange: (String) -> Unit) {
+    val imageModifier = Modifier
+        .size(250.dp)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = com.grayseal.notesapp.R.drawable.welcome),
+            contentDescription = "Splash Screen Image",
+            modifier = imageModifier
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text("This notes book belongs to", style = (TextStyle(fontSize = 18.sp, color = Color.Black)), fontFamily = sonoFamily, fontWeight = FontWeight.Normal)
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            modifier = Modifier.padding(start = 40.dp),
+            value = name,
+            onValueChange = onNameChange,
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.size(60.dp),
+                    imageVector = Icons.Outlined.EmojiNature,
+                    contentDescription = "Nature Icon",
+                )
+            },
+            singleLine = true,
+            textStyle = TextStyle(textIndent = TextIndent(12.sp), fontSize = 18.sp, color = MaterialTheme.colors.onBackground, fontFamily = sonoFamily, fontWeight = FontWeight.Normal),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Ascii, imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions.Default,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                textColor = Color.LightGray,
+                focusedBorderColor = Color(0xFFdaaac0),
+                unfocusedBorderColor = Color(0xFF4c6569),
+                leadingIconColor = Color(0xFFdaaac0),
+                cursorColor = Color(0xFF4c6569),
+            )
+
+        )
+    }
 }
