ff18d47f609bbe251f138219f3332f0620e410de
lynnemunini <muninilynne65@gmail.com>
2022-11-15 19:11:12 +0300

    Add data model and implement lazy column to display the notes

---
+++ app/src/main/java/com/grayseal/notesapp/model/notes.kt
@@ -0,0 +1,36 @@
+package com.grayseal.notesapp.model
+
+data class Note(
+    val id: String,
+    val title: String,
+    val date: String,
+    val note: String
+)
+
+val notes = listOf(
+        Note(
+            id = "1",
+            title = "Mobile Applications Assignment",
+            date = "15 Nov 2022",
+            note = "Remember to complete mobile applications development assignment before 26th November"
+        ),
+        Note(
+            id = "1",
+            title = "Mobile Applications Assignment",
+            date = "15 Nov 2022",
+            note = "Remember to complete mobile applications development assignment before 26th November"
+        ),
+        Note(
+            id = "1",
+            title = "Mobile Applications Assignment",
+            date = "15 Nov 2022",
+            note = "Remember to complete mobile applications development assignment before 26th November"
+        ),
+        Note(
+            id = "1",
+            title = "Mobile Applications Assignment",
+            date = "15 Nov 2022",
+            note = "Remember to complete mobile applications development assignment before 26th November"
+        ),
+
+    )
--- app/src/main/java/com/grayseal/notesapp/screens/HomeScreen.kt
+++ app/src/main/java/com/grayseal/notesapp/screens/HomeScreen.kt
@@ -1,8 +1,58 @@
 package com.grayseal.notesapp.screens
 
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.grayseal.notesapp.model.Note
+import com.grayseal.notesapp.model.notes
 
 @Composable
 fun HomeScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Transparent, elevation = 0.dp, modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center){
+                Text(text = "Lynne")
+            }
+        }
+    }) {
+        HomeContent(navController = navController)
+    }
+}
+
+@Composable
+fun HomeContent(navController: NavController) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn{
+            items(notes){
+                NoteCard(note = it)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteCard(note: Note){
+    Card(onClick = { /* Do something */ },
+        modifier = Modifier.size(width = 180.dp, height = 100.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text(note.title)
+        }
+    }
 }
