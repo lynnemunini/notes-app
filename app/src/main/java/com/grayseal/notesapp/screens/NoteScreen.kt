aefc70a90287f5a483abf9e48180a54f25595c17
lynnemunini <muninilynne65@gmail.com>
2022-11-16 14:40:45 +0300

    Change Note screen arrangement

--- app/src/main/java/com/grayseal/notesapp/screens/NoteScreen.kt
+++ app/src/main/java/com/grayseal/notesapp/screens/NoteScreen.kt
@@ -1,9 +1,6 @@
 package com.grayseal.notesapp.screens
 
-import android.widget.Toast
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -11,15 +8,14 @@ import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LeadingIconTab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.LeadingIconTab
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -27,7 +23,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.grayseal.notesapp.R
 import com.grayseal.notesapp.navigation.NoteScreens
 import com.grayseal.notesapp.ui.theme.sonoFamily
 import java.time.LocalDateTime
@@ -43,18 +38,33 @@ fun NoteScreen(navController: NavController) {
 fun NoteContent(navController: NavController) {
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxSize()) {
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 30.dp), horizontalArrangement = Arrangement.Center) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp), horizontalArrangement = Arrangement.Center
+            ) {
                 LeadingIconTab(
                     selected = false,
                     enabled = false,
                     onClick = { /*TODO*/ },
                     icon = {
-                        Icon(modifier = Modifier.size(60.dp), imageVector = Icons.Outlined.EmojiNature, contentDescription = "Notes", tint = Color(0xFFefcd95).copy(alpha = 0.9f))
+                        Icon(
+                            modifier = Modifier.size(60.dp),
+                            imageVector = Icons.Outlined.EmojiNature,
+                            contentDescription = "Notes",
+                            tint = Color(0xFFefcd95).copy(alpha = 0.9f)
+                        )
                     },
                     text = {
-                        Text("Note", color = Color(0xFFefcd95).copy(alpha = 0.9f), style = (TextStyle(fontSize = 30.sp, fontFamily = sonoFamily, fontWeight = FontWeight.Bold)))
+                        Text(
+                            "Note",
+                            color = Color(0xFFefcd95).copy(alpha = 0.9f),
+                            style = (TextStyle(
+                                fontSize = 30.sp,
+                                fontFamily = sonoFamily,
+                                fontWeight = FontWeight.Bold
+                            ))
+                        )
                     })
             }
             NoteArea(navController = navController)
@@ -63,42 +73,69 @@ fun NoteContent(navController: NavController) {
 }
 
 @Composable
-fun NoteArea(navController: NavController){
+fun NoteArea(navController: NavController) {
     var note by remember {
         mutableStateOf("")
     }
     var title by remember {
         mutableStateOf("")
     }
-    SaveButton(navController =navController, title, note)
-    Note(title = title, note = note, onTitleChange = { title = it },  onNoteChange = { note = it })
+    SaveButton(navController = navController, title, note)
+    Note(title = title, note = note, onTitleChange = { title = it }, onNoteChange = { note = it })
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun Note(title: String, note: String, onTitleChange: (String) -> Unit, onNoteChange: (String) -> Unit){
+fun Note(
+    title: String,
+    note: String,
+    onTitleChange: (String) -> Unit,
+    onNoteChange: (String) -> Unit
+) {
     val keyboardController = LocalSoftwareKeyboardController.current
 
     /*Date Row*/
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(top = 30.dp, bottom = 10.dp, start = 15.dp), horizontalArrangement = Arrangement.Start) {
-        Text(getCurrentDate(), style = (TextStyle(fontSize = 18.sp, color = Color(0xFFefcd95))), fontFamily = sonoFamily, fontWeight = FontWeight.Normal)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 30.dp, bottom = 10.dp, start = 15.dp),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Text(
+            getCurrentDate(),
+            style = (TextStyle(fontSize = 18.sp, color = Color(0xFFefcd95))),
+            fontFamily = sonoFamily,
+            fontWeight = FontWeight.Normal
+        )
     }
 
     /*Note Title Row*/
-    Row(horizontalArrangement = Arrangement.Start){
+    Row(horizontalArrangement = Arrangement.Start) {
         TextField(
             modifier = Modifier
                 .fillMaxWidth(),
             value = title,
             onValueChange = onTitleChange,
             placeholder = {
-                Text(text = "Note Title",color = Color.DarkGray, fontSize = 18.sp, fontFamily = sonoFamily, fontWeight = FontWeight.Normal)
+                Text(
+                    text = "Note Title",
+                    color = Color.DarkGray,
+                    fontSize = 18.sp,
+                    fontFamily = sonoFamily,
+                    fontWeight = FontWeight.Normal
+                )
             },
             singleLine = true,
-            textStyle = TextStyle(fontSize = 18.sp, color = Color.DarkGray, fontFamily = sonoFamily, fontWeight = FontWeight.Normal),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Ascii, imeAction = ImeAction.Done),
+            textStyle = TextStyle(
+                fontSize = 18.sp,
+                color = Color.DarkGray,
+                fontFamily = sonoFamily,
+                fontWeight = FontWeight.Normal
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Ascii,
+                imeAction = ImeAction.Done
+            ),
             keyboardActions = KeyboardActions {
                 keyboardController?.hide()
             },
@@ -114,18 +151,32 @@ fun Note(title: String, note: String, onTitleChange: (String) -> Unit, onNoteCha
     }
 
     /*Note Description Row*/
-    Row(horizontalArrangement = Arrangement.Start){
+    Row(horizontalArrangement = Arrangement.Start) {
         TextField(
             modifier = Modifier
                 .fillMaxWidth(),
             value = note,
             onValueChange = onNoteChange,
             placeholder = {
-                Text(text = "Write down something...",color = Color.LightGray, fontSize = 18.sp, fontFamily = sonoFamily, fontWeight = FontWeight.Light)
+                Text(
+                    text = "Write down something...",
+                    color = Color.LightGray,
+                    fontSize = 18.sp,
+                    fontFamily = sonoFamily,
+                    fontWeight = FontWeight.Light
+                )
             },
             singleLine = false,
-            textStyle = TextStyle(fontSize = 18.sp, color = colors.onBackground, fontFamily = sonoFamily, fontWeight = FontWeight.Normal),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Ascii, imeAction = ImeAction.Done),
+            textStyle = TextStyle(
+                fontSize = 18.sp,
+                color = colors.onBackground,
+                fontFamily = sonoFamily,
+                fontWeight = FontWeight.Normal
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Ascii,
+                imeAction = ImeAction.Done
+            ),
             keyboardActions = KeyboardActions {
                 keyboardController?.hide()
             },
@@ -159,10 +210,10 @@ fun SaveButton(navController: NavController, title: String, note: String) {
             onClick = {
                 if (title.isNotEmpty() && note.isNotEmpty()) {
                     navController.navigate(route = NoteScreens.HomeScreen.name)
-                }
-                else {
+                } else {
                     openDialog = true
-                } },
+                }
+            },
             enabled = true,
             contentPadding = PaddingValues(5.dp),
             colors = ButtonDefaults.textButtonColors(contentColor = Color(0xFFdaaac0))
@@ -178,7 +229,7 @@ fun SaveButton(navController: NavController, title: String, note: String) {
 }
 
 @Composable
-fun AlertDialog(openDialog: Boolean, onDismiss: () -> Unit){
+fun AlertDialog(openDialog: Boolean, onDismiss: () -> Unit) {
     if (openDialog) {
         AlertDialog(
             /* Dismiss the dialog when the user clicks outside the dialog or on the back
@@ -203,11 +254,23 @@ fun AlertDialog(openDialog: Boolean, onDismiss: () -> Unit){
                 }
             },
             text = {
-                Text(modifier = Modifier.padding(start = 5.dp), text = "Please enter Note title and note", color = colors.onBackground, fontSize = 18.sp, fontFamily = sonoFamily, fontWeight = FontWeight.Normal)
+                Text(
+                    modifier = Modifier.padding(start = 5.dp),
+                    text = "Don't forget to enter your Note Title and Note",
+                    color = colors.onBackground,
+                    fontSize = 16.sp,
+                    fontFamily = sonoFamily,
+                    fontWeight = FontWeight.Normal
+                )
             },
             confirmButton = {
                 TextButton(onClick = onDismiss) {
-                    Text("OK", style = (TextStyle(color = Color(0xFFdaaac0), fontSize = 20.sp)), fontFamily = sonoFamily, fontWeight = FontWeight.Bold)
+                    Text(
+                        "OK",
+                        style = (TextStyle(color = Color(0xFFdaaac0), fontSize = 20.sp)),
+                        fontFamily = sonoFamily,
+                        fontWeight = FontWeight.Bold
+                    )
                 }
             }
         )
